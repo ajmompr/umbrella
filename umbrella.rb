38a1efedcded9fc38105572f8db6b970f8ced774
Alexandra Mompremier <58926246+ajmompr@users.noreply.github.com>
2023-09-16 02:45:55 +0000

    assemble google geocode URL

--- umbrella.rb
+++ umbrella.rb
@@ -1,6 +1,12 @@
 require "http"
 require "json"
 
+#Get user location
 puts "Where are you?"
 location = gets.chomp
 pp "You are currently located in #{location}."
+
+
+# Assemble google geocode API URL
+google_maps_url = "https://maps.googleapis.com/maps/api/geocode/json?address=" + location + "&key=" + ENV.fetch("GMAPS_KEY")
+
