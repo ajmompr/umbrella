2521d143a6942ff1256d0f836000641ad33824ee
Alexandra Mompremier <58926246+ajmompr@users.noreply.github.com>
2023-09-16 02:49:35 +0000

    created raw_response to store http get request & parsed_response to store HTTP Response

--- umbrella.rb
+++ umbrella.rb
@@ -10,3 +10,10 @@ pp "You are currently located in #{location}."
 # Assemble google geocode API URL
 google_maps_url = "https://maps.googleapis.com/maps/api/geocode/json?address=" + location + "&key=" + ENV.fetch("GMAPS_KEY")
 
+# Place GET request to the google geocode API URL
+raw_response = HTTP.get(google_maps_url)
+
+# Parse the HTTP Response with JSON
+parsed_response = JSON.parse(raw_response)
+
+#pp parsed_response
